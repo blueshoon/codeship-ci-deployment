1e3989197c4a53780cec25b71b3743d1f6b0b686
Frederick Wells <frederickwells@Fredericks-iMac.local>
2019-02-14 11:20:35 -0600

    switch env for deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,8 @@ if [ "$CI_BRANCH" == "master" ]
 then
     repo=production
 else
-    repo=staging
+    # repo=staging
+    repo=preoduction
 fi
 
 if [[ "$CI_BRANCH" == "qa" && -n "$WPE_QA_INSTALL" ]]
