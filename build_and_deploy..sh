aec7f3a2e0517a96b7ca5568cf6efdbff08cd987
Frederick Wells <frederickwells@Fredericks-iMac.local>
2019-02-25 17:04:53 -0600

    new build code

---
+++ build_and_deploy..sh
@@ -0,0 +1,18 @@
+# Build
+yarn
+yarn run build:production
+# activate theme and build blade templates
+cd ../../..
+phpenv local 7.2
+php -d memory_limit=512M ~/wp-cli.phar --allow-root theme activate $FOLDERNAME
+php -d memory_limit=512M ~/wp-cli.phar --allow-root blade compile
+
+# for some reason this command fails the first time, runs ok the second
+php -d memory_limit=512M ~/wp-cli.phar --allow-root blade compile
+
+# Deploy
+git add --all :/
+git commit -m "DEPLOYMENT"
+git push servers HEAD:master --force
+# If this is the first time you're running the deployment, you might try this next line instead in case you get a missing branch error:
+# git push servers HEAD:refs/heads/master --force
